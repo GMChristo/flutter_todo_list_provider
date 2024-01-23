40f8951220e8dc8cfe4e725f43960d400cbcb83c
GMChristo <gabrielchristo@hotmail.com>
2024-01-22 21:26:32 -0300

    inicinado projeto de app To Do List usando provider

---
+++ android/app/src/main/kotlin/br/com/gmchristo/flutter_to_do_list_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gmchristo.flutter_to_do_list_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list_provider/app/app_module.dart';
+
+void main() {
+  runApp(const AppModule());
+}
